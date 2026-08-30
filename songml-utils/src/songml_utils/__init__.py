44fc918e59fb0ddaf9789a042ccae54826888d3a
Les Matheson <les.matheson@gmail.com>
2026-08-30 20:16:31 +0000

    Bump to 0.1.8

--- songml-utils/src/songml_utils/__init__.py
+++ songml-utils/src/songml_utils/__init__.py
@@ -1,6 +1,6 @@
 """SongML utilities - parser, formatter, and tools for working with SongML files."""
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 # Core exports
 from .abc_exporter import export_abc, to_abc_string
